4e7c40f7d08f84fc71a6377175c0eb3c7990b552
Juanma <juancrossetto@gmail.com>
2022-02-18 13:32:55 -0300

    test with console 2

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse): Promise<void> {
   console.log("var:", process.env.REVALIDATE_SECRET);
-  console.log("header:", req.headers['x-secret']);
+  console.log("header:", req.headers);
   console.log("equal?:", req.headers['x-secret'] === process.env.REVALIDATE_SECRET);
   if (req.headers['x-secret'] === process.env.REVALIDATE_SECRET) {
     await res.unstable_revalidate("/");
